d021fc3201c6ceda7e7c9f78ee821c19556c10c9
Markus Neubauer <neubauer@email-online.org>
2014-06-08 17:57:36 +0200

    changed to support localization and the new templates default and larry

--- login_info.php
+++ login_info.php
@@ -18,24 +18,45 @@ class login_info extends rcube_plugin
     public $noajax = true;
     // skip frames
     public $noframe = true;
+
+    private $login_info = '';
+    private $bottomline = '';
     private $addstr = '';
                         
         function init()
         {
-                $this->rcmail = rcube::get_instance();
-                $this->load_config();
                 $this->add_texts ('localization', false);
                 $this->add_hook('template_object_loginform', array($this, 'add_login_info'));
         }
 
         public function add_login_info($arg)
         {
-                if ( $this->gettext('custom_login_info') ) {
-                        $addstr = $this->gettext('custom_login_info');
+                $rcmail = rcube::get_instance();
+                $this->load_config();
+
+                // order: 1st config.inc.php then localization
+                if ( $rcmail->config->get('custom_login_info') ) 
+                        $login_info = $rcmail->config->get('custom_login_info');
+                elseif ( $this->gettext('custom_login_info') ) {
+                        $login_info = $this->gettext('custom_login_info');
                 }
-                elseif ( $rcmail->config->get('login_info') ) 
-                        $addstr = $rcmail->config->get('login_info');
-                rcmail::get_instance()->output->add_footer( $addstr );
+
+                // order: 1st config.inc.php then localization
+                if ( $rcmail->config->get('custom_login_bottomline') ) 
+                        $bottomline = $rcmail->config->get('custom_login_bottomline');
+                elseif ( $this->gettext('custom_login_bottomline') ) {
+                        $bottomline = $this->gettext('custom_login_bottomline');
+                }
+                
+                // use exitings id's message and bottomline
+                if ( !empty($login_info) || !empty($bottomline) ) {
+                    $addstr = '<script type="text/javascript">';
+                    if (!empty($login_info) ) $addstr .= 'document.getElementById("message").innerHTML = \''.$login_info.'\';';
+                        
+                    if (!empty($bottomline) ) $addstr .= 'document.getElementById("bottomline").innerHTML = \''.$bottomline.'\';';
+                    $addstr .= '</script>';
+                    $rcmail->output->add_footer( $addstr );
+                }
 
                 return $arg;
         }
