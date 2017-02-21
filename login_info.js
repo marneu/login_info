d9575b95cc16c58c61971d83e90066f802b34c78
Markus Neubauer <neubauer@email-online.org>
2017-02-21 07:05:43 +0100

    avoid undefined errors

--- login_info.js
+++ login_info.js
@@ -4,24 +4,24 @@
  *
  * Jscript (jquery) for login_info plugin to add a customized info to the login screen
  *
- * @version 1.4
+ * @version 1.4.1
  * @author Markus Neubauer
  * @http://www.std-soft.com/index.php/hm-service/81-c-std-service-code/2-text-auf-der-login-seite-einblenden
  * @example: https://www.std-soft.de/webmail (in production)
  */
 
-if ( login_info_before != null ) {
+if ( 'undefined' !== typeof login_info_before && login_info_before !== null ) {
     $(document).ready(function() {
       $('#login-form').html(login_info_before.concat($('#login-form').html()));
     });
 }
-if ( login_info_after != null ) {
+if ( 'undefined' !== typeof login_info_after && login_info_after !== null ) {
     $(document).ready(function() {
       $('#login-form').after(login_info_after);
     });
 }
 
-if ( bottomline != null ) {
+if ( 'undefined' !== typeof bottomline && bottomline !== null ) {
   // larry
   if ( document.getElementById('bottomline') ) {
       $(document).ready(function() {
@@ -35,5 +35,4 @@ if ( bottomline != null ) {
       });
   }
 }
-
 
